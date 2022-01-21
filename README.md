dae2b0fedfa525cb7eff2f748a7221d3fe527e70
Liu-Chunzhang <lkz9808@163.com>
2022-01-21 20:15:53 +0800

    description

---
+++ README.md
@@ -0,0 +1,2 @@
+# matplotlib_practice
+This is where the code for the book matplotlib_practice is stored.
--- 第1篇 入门/第2章 使用统计函数绘制简单图形/2.07 函数stem()——用于绘制棉棒图/2.7.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0.5, 2*np.pi, 20)
-y = np.random.randn(20)
-
-plt.stem(x, y, linefmt="-.", markerfmt="o", basefmt="-")
-
-plt.show()
