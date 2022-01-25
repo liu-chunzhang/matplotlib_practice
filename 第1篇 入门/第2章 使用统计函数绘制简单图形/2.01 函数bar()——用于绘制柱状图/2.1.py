0d9e7f85db5c17944e63f8fe15e59da3ebc8674c
Liu-Chunzhang <lkz9808@163.com>
2022-01-25 18:05:47 +0800

    chapter-02

--- 第1篇 入门/第2章 使用统计函数绘制简单图形/2.01 函数bar()——用于绘制柱状图/2.1.py
+++ 第1篇 入门/第2章 使用统计函数绘制简单图形/2.01 函数bar()——用于绘制柱状图/2.1.py
@@ -4,8 +4,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-mpl.rcParams["font.sans-serif"]=["SimHei"]	# 使用中文字体
-mpl.rcParams["axes.unicode_minus"]=False	# 允许使用负号
+mpl.rcParams["font.sans-serif"] = ["SimHei"]	# 使用中文字体
+mpl.rcParams["axes.unicode_minus"] = False		# 不使用默认处理负号的方式
 
 # some simple data
 x = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -13,11 +13,12 @@ y = [3, 1, 4, 5, 8, 9, 7, 2]
 
 # create bar
 plt.bar(x, y, align="center", color='c', tick_label=["q", "a", "c", "e", "r", "j", "b", "p"], hatch="/", linewidth=1, edgecolor='k')
+																																# 用于绘制柱状图
 
 # set x,y_axis label
 plt.xlabel("箱子编号")
-plt.ylabel("箱子重量(kg)")
 plt.ylim(0.0, 9.0)
-plt.yticks(np.arange(0.0, 9.1))
+plt.locator_params(axis='y', nbins=10)
+plt.ylabel("箱子重量(kg)")
 
 plt.show()
