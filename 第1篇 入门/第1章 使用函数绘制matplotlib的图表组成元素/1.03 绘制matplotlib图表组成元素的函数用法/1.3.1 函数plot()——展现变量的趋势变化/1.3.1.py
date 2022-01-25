1989b05dd92c8eab81a66b5c54d62318f1aa2d37
Liu-Chunzhang <lkz9808@163.com>
2022-01-25 16:46:27 +0800

    chapter-01

--- 第1篇 入门/第1章 使用函数绘制matplotlib的图表组成元素/1.03 绘制matplotlib图表组成元素的函数用法/1.3.1 函数plot()——展现变量的趋势变化/1.3.1.py
+++ 第1篇 入门/第1章 使用函数绘制matplotlib的图表组成元素/1.03 绘制matplotlib图表组成元素的函数用法/1.3.1 函数plot()——展现变量的趋势变化/1.3.1.py
@@ -3,14 +3,14 @@ import numpy as np
 
 x = np.linspace(0.05, 10, 1000)
 y = np.cos(x)
-plt.plot(x, y, ls='-', lw=2, label='plot figure')
+plt.plot(x, y, ls='-', lw=2, label='plot figure')	# 展现变量的趋势变化
 
 plt.legend(loc="upper right")
 
-# some extra configure
-plt.xticks(np.arange(0.0, 11.0, 2.0))
+# some extra configure					# 这些语句为保持图片拉伸时刻度的不变性而添加，之后文件同
 plt.xlim(0.0, 10.0)
-plt.yticks(np.arange(-1.0, 1.1, 0.5))
+plt.locator_params(axis='x', nbins=6)
 plt.ylim(-1.0, 1.0)
+plt.locator_params(axis='y', nbins=5)
 
 plt.show()
--- 第1篇 入门/第1章 使用函数绘制matplotlib的图表组成元素/1.03 绘制matplotlib图表组成元素的函数用法/1.3.2 函数scatter()——寻找变量之间的联系/1.3.2.py
+++ 第1篇 入门/第1章 使用函数绘制matplotlib的图表组成元素/1.03 绘制matplotlib图表组成元素的函数用法/1.3.2 函数scatter()——寻找变量之间的联系/1.3.2.py
@@ -4,13 +4,13 @@ import numpy as np
 x = np.linspace(0.05, 10, 1000)
 y = np.random.rand(1000)
 
-plt.scatter(x, y, label="scatter figure")
+plt.scatter(x, y, c='b', label="scatter figure")	# 寻找变量之间的关系
 
 plt.legend(loc="upper right")
 
 plt.xlim(-2.0, 12.0)
-plt.xticks(np.arange(-2.0, 12.1, 2.0))
+plt.locator_params(axis='x', nbins=7)
 plt.ylim(-0.2, 1.2)
-plt.yticks(np.arange(-0.2, 1.3, 0.2))
+plt.locator_params(axis='y', nbins=8)
 
 plt.show()
