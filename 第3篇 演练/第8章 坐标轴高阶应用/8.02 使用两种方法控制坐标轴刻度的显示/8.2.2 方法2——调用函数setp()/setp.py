c7b2c3849aafd0ec536970da82d9c4e97dc19dc7
Liu-Chunzhang <lkz9808@163.com>
2022-01-25 16:49:20 +0800

    none

--- 第3篇 演练/第8章 坐标轴高阶应用/8.02 使用两种方法控制坐标轴刻度的显示/8.2.2 方法2——调用函数setp()/setp.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-line = plt.plot([1, 2, 3])
-plt.setp(line, ls="--")
-plt.show()
--- 第4篇 拓展/第12章 颜色使用/12.02 综合案例/12.2.1 案例1——模拟图的颜色使用模式/12.2.1.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-rd = np.random.rand(10, 10)
-plt.pcolor(rd, cmap="BuPu")
-plt.colorbar()
-
-plt.show()
