6587611999867a8d7b2cbcdaaf25b12d7c868a3e
Liu-Chunzhang <lkz9808@163.com>
2022-01-27 22:00:12 +0800

    section-2

--- 第2篇 精进/第6章 划分画布的主要函数/6.03 函数subplots()：创建一张画布带有多个子区的绘图模式/6.3.2 案例2——创建一张画布和两个子区的绘图模式/6.3.2.py
+++ 第2篇 精进/第6章 划分画布的主要函数/6.03 函数subplots()：创建一张画布带有多个子区的绘图模式/6.3.2 案例2——创建一张画布和两个子区的绘图模式/6.3.2.py
@@ -19,14 +19,21 @@ fig, ax = plt.subplots(1, 2, sharey=True)
 # subplot(121)
 ax1 = ax[0]
 ax1.plot(x, y, "k--", lw=2)
+
+ax1.set_xlim(0, 7)
+ax1.locator_params(axis='x', nbins=8)
+ax1.set_ylim(-0.05, 0.35)
+ax1.locator_params(axis='y', nbins=9)
+
 ax1.set_title("折线图")
 ax1.grid(ls=":", lw=1, color="gray", alpha=0.8)
-ax1.set_xlim(0, 7)
-ax1.set_ylim(-0.05, 0.35)
 
 # subplot(122)
 ax2 = ax[1]
-ax2.scatter(x, y, s=10, c="skyblue", marker="o")
+ax2.scatter(x, y, s=10, marker="o", facecolor='w', edgecolor='skyblue')	# 此处教材错误！因需要空心圆，所以不能用color，而应该分别设置facecolor和edgecolor！
+
+ax2.set_xlim(-1, 7)
+ax2.locator_params(axis='x', nbins=9)
 ax2.set_title("散点图")
 
 # Create a figure title
