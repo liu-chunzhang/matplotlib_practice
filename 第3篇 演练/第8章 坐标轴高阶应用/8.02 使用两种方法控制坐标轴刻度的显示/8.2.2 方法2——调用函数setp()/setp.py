2cac55e12739468cab85b979de91b09b30b3db03
Liu-Chunzhang <lkz9808@163.com>
2022-01-29 21:00:38 +0800

    none

--- 第3篇 演练/第8章 坐标轴高阶应用/8.02 使用两种方法控制坐标轴刻度的显示/8.2.2 方法2——调用函数setp()/setp.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-line, = plt.plot([1, 2, 3])		# 此处逗号不可不要，表明line是Line2D对象
-plt.setp(line, ls="--")
-plt.show()
--- 第4篇 拓展/第10章 matplotlib的配置/10.01 修改代码层面的matplotlib的配置/10.1.1 方法1——调用函数matplotlib.rc()/10.1.1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib as mpl
-
-# usage of package matplotlib
-## method1 of setting attribution
-mpl.rc("lines", linewidth=2, color='c', linestyle="--")		# 调用函数matplotlib.rc()
-
-## method2 of setting atrribution
-line = {"linewidth":2, "color":"c", "linestyle":"--"}
-mpl.rc("lines", **line)
